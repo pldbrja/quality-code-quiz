a71234383d4e50c3a4cae2ceb8a211f1597a2b18
Mia Borja <apk.borja@gmail.com>
2022-03-14 21:53:14 -0700

    Updated index.html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
+var beginButton = document.querySelector("#starter")
+
+var userScore = [];
 
 var fullscrBG = setTimeout(function() {
     document.querySelector("body").setAttribute("style", "background: rgb(236, 236, 236); transition: .5s background ease-in;")
 }, 1000);
 
 var contentLD = setTimeout(function () {
-    document.querySelector("")
-})
+    document.querySelector("#playbox").setAttribute("style","opacity: 1; animation: fadeIn .3s ease-in; ")
+}, 1700);
 
 
+beginButton.addEventListener("click", function (event) {
+    event.preventDefault;
+    ADDTOTHIS;
+});
 fullscrBG();
