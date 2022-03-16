76537145652caa11a4938d4d788d215342e4d03f
Mia Borja <apk.borja@gmail.com>
2022-03-15 20:37:39 -0700

    Updated variables for changing question.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-var beginButton = document.querySelector("#starter")
+var initialsInput = document.querySelector("name");
+var finalScore = 0;
 
-var userScore = [];
+var userScore = {
+    finalCount: finalScore.value,
+};
 
-var fullscrBG = setTimeout(function() {
+var quizQuestions = document.querySelectorAll('#playQuest');
+
+const questions = Array.from(quizQuestions);
+
+console.log(questions);
+
+setTimeout(function fullscrBG() {
     document.querySelector("body").setAttribute("style", "background: rgb(236, 236, 236); transition: .5s background ease-in;")
 }, 1000);
 
 var contentLD = setTimeout(function () {
     document.querySelector("#playbox").setAttribute("style","opacity: 1; animation: fadeIn .3s ease-in; ")
 }, 1700);
-
-
-beginButton.addEventListener("click", function (event) {
-    event.preventDefault;
-    ADDTOTHIS;
-});
-fullscrBG();
